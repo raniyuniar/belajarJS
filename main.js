e1bc9764600da2d75e5b9aa81d947e978d6b410d
Rani <LENOVO@Raniyuniar>
2023-10-12 17:22:34 +0700

    update index.html

---
+++ main.js
@@ -0,0 +1,52 @@
+//console.log ("hello world")
+//alert("ada notifikasi")
+//prompt("pinjem duls donk")
+
+//var promnet = "coding is easy"
+//console.log (promnet)
+//var promnet = "coding not easy"
+//console.log (promnet)
+//var promnet = " kadang gampang"
+//console.log (promnet)
+
+//let promnet = "coding bikin happy"
+//console.log(promnet)
+//let promnet = "coding bikin happy"
+//console.log(promnet)
+
+//const promnet = "coding kecintaan aku"
+//console.log(promnet)
+//const promnet = "coding bukan kecintaan aku"
+//console.log(promnet)
+
+//let totalPoin = prompt("masukan poin anda")
+//if(totalPoin >= 100){
+//  document.write("wuhuuuu selamat yachhh");
+//}else{
+// document.write("gafafah semangat");
+//}
+
+//let x = 6; 
+//let y = 3;
+
+//console.log(x<10 && y > 1);
+//console.log(x<10 && y < 1);
+//console.log(x==5||y==5);
+//console.log(x==6||y==5);
+//console.log(!(x==y));
+
+//document.write(x<10 && y > 1);
+//document.write("<br>");
+//document.write(x<10 && y < 1);
+
+let p = document.querySelector("p")
+let button = document.querySelector("button")
+let input = document.querySelector("input")
+button.addEventListener('click', function(){
+    let isi = input.value
+    console.log(isi)
+    p.innerText = isi
+})
+ 
+
+
